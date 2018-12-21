ea3aa248a35c0d8357b8e981f668c014e0af9679
jim380 <jyz1992@hotmail.com>
2018-12-21 21:00:31 +0000

    minor changes

--- Cosmos/CLI/auto_delegate.sh
+++ Cosmos/CLI/auto_delegate.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+pause(){
+ read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+}
 while true
         do
+                CHAINID=game_of_stakes_1
                 read -s -p "Passphrase: " passphrase
                 # echo $passphrase|gaiacli tx dist withdraw-rewards --chain-id "genki-2000" --from "main" --is-validator
                 read -p "
 Fee: " FEE
                 echo "Fee has been set to $FEE STAKE."
-                STEAK=`gaiacli query account --chain-id=game_of_stakes cosmos1pjmngrwcsatsuyy8m3qrunaun67sr9x78qhlvr --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
+                # STEAK=`gaiacli query account --chain-id=$CHAINID cosmos1pjmngrwcsatsuyy8m3qrunaun67sr9x78qhlvr --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
+                ASSET="`gaiacli query account --chain-id=$CHAINID cosmos1pjmngrwcsatsuyy8m3qrunaun67sr9x78qhlvr --trust-node | jq ".value.coins" | jq ".[0].denom"| bc`"
+                if [  $ASSET == "photinos" ]
+                then
+                        echo "No STAKE yet"  
+                        pause   
+                else
+                STEAK=`gaiacli query account --chain-id=$CHAINID cosmos1pjmngrwcsatsuyy8m3qrunaun67sr9x78qhlvr --trust-node | jq ".value.coins" | jq ".[0].amount" | bc`
                 # echo "$STEAK"
                 NETSTAKE=$(($STEAK - $FEE))
                 # echo "Net stake: $NETSTAKE"
@@ -18,12 +29,12 @@ Fee: " FEE
                                 echo "----------------------------------"
                                 echo " Withdraw "
                                 echo "----------------------------------"
-                                echo $passphrase|gaiacli tx dist withdraw-rewards --chain-id "game_of_stakes" --from "CypherCore" --is-validator
+                                echo $passphrase|gaiacli tx dist withdraw-rewards --chain-id=$CHAINID --from="CypherCore" --is-validator
                                 sleep 10s
                                 echo "----------------------------------"
                                 echo " Delegate "
                                 echo "----------------------------------"
-                                echo $passphrase|gaiacli tx stake delegate --from "CypherCore" --validator "cosmosvaloper1pjmngrwcsatsuyy8m3qrunaun67sr9x7z5r2qs" --chain-id "game_of_stakes" --amount "$NETSTAKE""STAKE" --sequence 0
+                                echo $passphrase|gaiacli tx stake delegate --from="CypherCore" --validator "cosmosvaloper1pjmngrwcsatsuyy8m3qrunaun67sr9x7z5r2qs" --chain-id "game_of_stakes" --amount "$NETSTAKE""STAKE" --sequence 0
                                 sleep 10s
                                 echo "----------------------------------"
                                 echo " Balance "
@@ -35,4 +46,5 @@ Fee: " FEE
                 echo "You only have $STEAK STAKE"
                 echo "----------------------------------"
                 sleep 12000
+                fi
         done
