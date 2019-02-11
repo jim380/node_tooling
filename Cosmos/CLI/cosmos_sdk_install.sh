4709f99eeb2dd19a5ad76a15a684727aabce8187
jim380 <jyz1992@hotmail.com>
2019-02-11 02:23:16 -0800

    bunch of fixes

--- Cosmos/CLI/cosmos_sdk_install.sh
+++ Cosmos/CLI/cosmos_sdk_install.sh
@@ -25,97 +25,124 @@
 #  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #  
 #  ============================================================================
-echo "-----------------------------------------"
-echo "               System Update             "
-echo "-----------------------------------------"
-sudo apt-get update && sudo apt-get upgrade -y 
-echo "-----------------------------------------"
-echo "                Preparation              "
-echo "-----------------------------------------"
-sudo apt-get install make gcc g++ -y
-sudo sudo apt autoremove
-echo "-----------------------------------------"
-echo "             Install Binaries            "
-echo "-----------------------------------------"
-if ! test -d $GOPATH/src/github.com/cosmos
-then
-    mkdir -p $GOPATH/src/github.com/cosmos
-    cd $GOPATH/src/github.com/cosmos
-    git clone https://github.com/cosmos/cosmos-sdk
-fi
-echo "-----------------------------------------"
-echo "             Update Binary               "
-echo "-----------------------------------------"
-REPO=$GOPATH/src/github.com/cosmos/cosmos-sdk
-if [ -d "$REPO" ]
-then
-    cd $REPO
-    if [ -d "$REPO/.git" ]
+function preparation {
+    echo "-----------------------------------------"
+    echo "               System Update             "
+    echo "-----------------------------------------"
+    sudo apt-get update && sudo apt-get upgrade -y 
+    echo "-----------------------------------------"
+    echo "                Preparation              "
+    echo "-----------------------------------------"
+    sudo apt-get install make gcc g++ -y
+    sudo sudo apt autoremove
+}
+
+function binary {
+    echo "-----------------------------------------"
+    echo "             Install Binaries            "
+    echo "-----------------------------------------"
+    if ! test -d $GOPATH/src/github.com/cosmos
     then
-      echo "Updating $REPO at
-`date`"
-      git status
-      echo "-----------------------------------------"
-      echo "               Fetching                  "
-      echo "-----------------------------------------"
-      git fetch
-      echo "-----------------------------------------"
-      echo "                Pulling                  "
-      echo "-----------------------------------------"
-      git pull
-    else
-      echo "-----------------------------------------"
-      echo "This is not a git folder."
-      echo "-----------------------------------------"
+        mkdir -p $GOPATH/src/github.com/cosmos
+        cd $GOPATH/src/github.com/cosmos
+        git clone https://github.com/cosmos/cosmos-sdk
     fi
-    echo "Finished updating at
-`date`"
-    echo ""
-fi
-echo "-----------------------------------------"
-echo "                 Checkout                "
-echo "-----------------------------------------"
-read -p "What version would you like to checkout?
-Enter 'master' or specify a version number (e.g. 'v0.29.0')
+    echo "-----------------------------------------"
+    echo "             Update Binary               "
+    echo "-----------------------------------------"
+    REPO=$GOPATH/src/github.com/cosmos/cosmos-sdk
+    if [ -d "$REPO" ]
+    then
+        cd $REPO
+        if [ -d "$REPO/.git" ]
+        then
+        echo "Updating $REPO at
+    `date`"
+        git status
+        echo "-----------------------------------------"
+        echo "               Fetching                  "
+        echo "-----------------------------------------"
+        git fetch
+        echo "-----------------------------------------"
+        echo "                Pulling                  "
+        echo "-----------------------------------------"
+        git pull
+        else
+        echo "-----------------------------------------"
+        echo "This is not a git folder."
+        echo "-----------------------------------------"
+        fi
+        echo "Finished updating at
+    `date`"
+        echo ""
+    fi
+    echo "-----------------------------------------"
+    echo "            Version Checkout             "
+    echo "-----------------------------------------"
+    read -p "What version would you like to checkout?
+Enter 'master' or specify a version number (e.g. 'v0.31.1')
 " CHECKOUT_VERSION
-echo "Installing $CHECKOUT_VERSION"
-git checkout $CHECKOUT_VERSION
-echo "-----------------------------------------"
-echo "              Make & Install             "
-echo "-----------------------------------------"
-make get_vendor_deps && make install
-echo "-----------------------------------------"
-echo "                Node Init                "
-echo "-----------------------------------------"
-read -p "What would you like the node to be called?
+    echo "Installing $CHECKOUT_VERSION"
+    git checkout $CHECKOUT_VERSION
+    echo "-----------------------------------------"
+    echo "              Make & Install             "
+    echo "-----------------------------------------"
+    make get_vendor_deps && make install
+}
+
+function init {
+    echo "-----------------------------------------"
+    echo "                Node Init                "
+    echo "-----------------------------------------"
+    read -p "What would you like the node to be called?
 " NODE_NAME
-echo "Node name has been set to '$NODE_NAME'"
-read -p "What would you like the Gaiad home directory to be? (default: ~/.gaiad)
-" GAIAD_HOME
-if [ -z "$GAIAD_HOME" ]
-then 
-    echo "Gaiad home directory has been set to ~/.gaiad"
-    gaiad init --name=$NODE_NAME --home=~/.gaiad
-else
-    echo "Gaiad home directory has been set to '$GAIAD_HOME'"
-    iris init --name=$NODE_NAME --home=$GAIAD_HOME
-fi
-echo "-----------------------------------------"
-echo "            Fetch genesis.json           "
-echo "-----------------------------------------"
-if test -d $GAIAD_HOME/config/genesis.json
-then
-    rm $GAIAD_HOME/config/genesis.json
-fi
-read -p "Link to genesis.json in raw format:
+    echo "Node name has been set to '$NODE_NAME'"
+    echo "What would you like the Gaiad home directory to be? (default: ~/.gaiad)"
+    read -p "Path: " GAIAD_HOME
+    if [ -z "$GAIAD_HOME" ]
+    then 
+        echo "Gaiad home directory has been set to ~/.gaiad"
+        gaiad init $NODE_NAME --home=~/.gaiad
+    else
+        echo "Gaiad home directory has been set to '$GAIAD_HOME'"
+        gaiad init $NODE_NAME --home=$GAIAD_HOME
+    fi
+}
+
+function genesis {
+    echo "-----------------------------------------"
+    echo "            Fetch genesis.json           "
+    echo "-----------------------------------------"
+    if test -d $GAIAD_HOME/config/genesis.json
+    then
+        rm $GAIAD_HOME/config/genesis.json
+    fi
+    read -p "Link to genesis.json in raw format:
 " GENESIS
+    echo ""
+    curl $GENESIS > $GAIAD_HOME/config/genesis.json
+}
+
+function version {
+    echo "-----------------------------------------"
+    echo "               gaiad version             "
+    echo "-----------------------------------------"
+    gaiad version
+    echo "-----------------------------------------"
+    echo "              gaiacli version"
+    echo "-----------------------------------------"
+    gaiacli version
+}
+
+preparation
+binary
 echo ""
-curl $GENESIS > $GAIAD_HOME/config/genesis.json
-echo "-----------------------------------------"
-echo "               gaiad version             "
-echo "-----------------------------------------"
-gaiad version
-echo "-----------------------------------------"
-echo "              gaiacli version"
-echo "-----------------------------------------"
-gaiacli version
+read -p "Would you like to init a new node? (y/n) " ans
+case "$ans" in 
+    "y"|"yes"|"Y"|"Yes"|"YES") init
+    ;;
+    *) echo "Continue..."
+    ;;
+esac
+genesis
+version
--- Cosmos/CLI/upgrade.sh
+++ Cosmos/CLI/upgrade.sh
@@ -25,90 +25,97 @@
 #  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #  
 #  ============================================================================
-if test -e $HOME/.gaiad/config/addrbook.json && test -e $HOME/.gaiad/config/genesis.json
-then
-    echo "Remove addrbook.json and genesis.json"
-    echo "-----------------------------------------"
-    rm $HOME/.gaiad/config/addrbook.json $HOME/.gaiad/config/genesis.json
-fi
-if [ -e "$GOPATH/bin/gaiad" ]
-then
-    echo "-----------------------------------------"
-    echo "                 Reset                   "
-    echo "-----------------------------------------"
-    gaiad unsafe-reset-all
-fi
-if test -e Gopkg.lock
-then
-    echo "-----------------------------------------"
-    echo "              rm Gopkg.lock              "
-    echo "-----------------------------------------"
-    rm Gopkg.lock 
-fi
-echo "-----------------------------------------"
-echo "             Update Binary               "
-echo "-----------------------------------------"
-REPO=$GOPATH/src/github.com/cosmos/cosmos-sdk
-if [ -d "$REPO" ]
-then
-    cd $REPO
-    if [ -d "$REPO/.git" ]
+function preparation {
+    if test -e $HOME/.gaiad/config/addrbook.json && test -e $HOME/.gaiad/config/genesis.json
     then
-      echo "Updating $REPO at
-`date`"
-      git status
-      echo "-----------------------------------------"
-      echo "               Fetching                  "
-      echo "-----------------------------------------"
-      git fetch
-      echo "-----------------------------------------"
-      echo "                Pulling                  "
-      echo "-----------------------------------------"
-      git pull
-    else
-      echo "-----------------------------------------"
-      echo "This is not a git folder."
-      echo "-----------------------------------------"
+        echo "-----------------------------------------"
+        echo "Remove addrbook.json and genesis.json"
+        echo "-----------------------------------------"
+        rm $HOME/.gaiad/config/addrbook.json $HOME/.gaiad/config/genesis.json
     fi
-    echo "Finished updating at
+}
+
+function binary {
+    echo "-----------------------------------------"
+    echo "             Update Binary               "
+    echo "-----------------------------------------"
+    REPO=$GOPATH/src/github.com/cosmos/cosmos-sdk
+    if [ -d "$REPO" ]
+    then
+        cd $REPO
+        if [ -d "$REPO/.git" ]
+        then
+        echo "Updating $REPO at
 `date`"
-    echo ""
-  fi
-echo "-----------------------------------------"
-echo "                 Checkout                "
-echo "-----------------------------------------"
-test -z ${CHECKOUT_VERSION} && read -p "What version would you like to checkout?
+        git status
+        echo "-----------------------------------------"
+        echo "               Fetching                  "
+        echo "-----------------------------------------"
+        git fetch
+        echo "-----------------------------------------"
+        echo "                Pulling                  "
+        echo "-----------------------------------------"
+        git pull
+        else
+        echo "-----------------------------------------"
+        echo "This is not a git folder."
+        echo "-----------------------------------------"
+        fi
+        echo "Finished updating at
+`date`"
+        echo ""
+    fi
+    echo "-----------------------------------------"
+    echo "                 Checkout                "
+    echo "-----------------------------------------"
+    test -z ${CHECKOUT_VERSION} && read -p "What version would you like to checkout?
 Enter 'master' or specify a version number (e.g. 'v0.28.0')
 " CHECKOUT_VERSION
-echo "Installing $CHECKOUT_VERSION"
-git checkout $CHECKOUT_VERSION
-echo "-----------------------------------------"
-echo "              Make & Install             "
-echo "-----------------------------------------"
-make update_tools && make get_vendor_deps && make install
-if [ -e "$GOPATH/bin/gaiad" ]
-then
+    echo "Installing $CHECKOUT_VERSION"
+    git checkout $CHECKOUT_VERSION
     echo "-----------------------------------------"
-    echo "                 Reset                   "
+    echo "              Make & Install             "
     echo "-----------------------------------------"
-    gaiad unsafe-reset-all
-fi
-echo "-----------------------------------------"
-echo "            Fetch genesis.json           "
-echo "-----------------------------------------"
-test -z ${GENESIS} && read -p "Link to genesis.json in raw format
+    make update_tools && make get_vendor_deps && make install
+}
+
+function reset {
+    if [ -e "$GOPATH/bin/gaiad" ]
+    then
+        echo "-----------------------------------------"
+        echo "                 Reset                   "
+        echo "-----------------------------------------"
+        gaiad unsafe-reset-all
+    fi
+}
+
+function genesis {
+    echo "-----------------------------------------"
+    echo "            Fetch genesis.json           "
+    echo "-----------------------------------------"
+    test -z ${GENESIS} && read -p "Link to genesis.json in raw format
 " GENESIS
-echo ""
-if ! test -d $HOME/.gaiad/config
-then
-    mkdir -p $HOME/.gaiad/config
-fi
-curl $GENESIS > $HOME/.gaiad/config/genesis.json
-echo "-----------------------------------------"
-echo "               gaiad version             "
-echo "-----------------------------------------"
-gaiad version
-echo "-----------------------------------------"
-echo "              gaiacli version"
-echo "-----------------------------------------"
-gaiacli version
+    echo ""
+    if ! test -d $HOME/.gaiad/config
+    then
+        mkdir -p $HOME/.gaiad/config
+    fi
+    curl $GENESIS > $HOME/.gaiad/config/genesis.json
+}
+
+function version {
+    echo "-----------------------------------------"
+    echo "               gaiad version             "
+    echo "-----------------------------------------"
+    gaiad version
+    echo "-----------------------------------------"
+    echo "              gaiacli version"
+    echo "-----------------------------------------"
+    gaiacli version
+}
+
+preparation
+binary
+reset
+genesis
+version
