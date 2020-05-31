34668b63e4797d12df69b5e331d3ca4b47894086
jim380 <admin@cyphercore.io>
2020-05-30 23:22:44 -0700

    added security group and user data

--- terraform/aws/instance.tf
+++ terraform/aws/instance.tf
@@ -3,10 +3,20 @@ resource "aws_key_pair" "main_key" {
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "dev_server" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
   key_name      = aws_key_pair.main_key.key_name
+  # security group
+  security_groups = ["aws_security_group.allow-ssh.id"]
+  # user data
+  user_data = data.template_cloudinit_config.cloudinit.rendered
+
+  root_block_device {
+    volume_size = 500
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
 
   provisioner "file" {
     source      = "script.sh"
