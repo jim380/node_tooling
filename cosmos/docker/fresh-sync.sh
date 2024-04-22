76246b4216cf7a18583728f8b0685a596bf68180
Jay Jie <jyz1992@hotmail.com>
2024-04-22 01:23:37 -0700

    fix: fresh sync script

--- cosmos/docker/fresh-sync.sh
+++ cosmos/docker/fresh-sync.sh
@@ -32,22 +32,29 @@ awk -v rpc_laddr="$RPC_LADDR" '
   rpc && $1~/^laddr/ {$2=" \"" rpc_laddr "\""}
   {print $0}
 ' $NODE_HOME_DIR_LOCAL/config/config.toml > temp_config.toml && mv temp_config.toml $NODE_HOME_DIR_LOCAL/config/config.toml
+ awk -v p2p_laddr="$P2P_LADDR" '
+  BEGIN {OFS=FS="="}
+  /^\[p2p\]$/ {p2p=1}
+  p2p && $1~/^laddr/ {$2=" \"" p2p_laddr "\""; p2p=0}
+  {print $0}
+' $NODE_HOME_DIR_LOCAL/config/config.toml > temp_config.toml && mv temp_config.toml $NODE_HOME_DIR_LOCAL/config/config.toml
 
 # configure app.toml
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # mac
-  sed -i '' "s/minimum-gas-prices = \".*\"/minimum-gas-prices = \"$MIN_GAS_PRICE\"/" $NODE_HOME_DIR_LOCAL/config/app.toml
+  sed -i '' "s/minimum-gas-prices = \".*\"/minimum-gas-prices = \"$MIN_GAS_PRICE\"|" $NODE_HOME_DIR_LOCAL/config/app.toml
 else
   # linux
-  sed -i "s/minimum-gas-prices = \".*\"/minimum-gas-prices = \"$MIN_GAS_PRICE\"/" $NODE_HOME_DIR_LOCAL/config/app.toml
+  sed -i "s|minimum-gas-prices = \".*\"|minimum-gas-prices = \"$MIN_GAS_PRICE\"|" $NODE_HOME_DIR_LOCAL/config/app.toml
 fi
 awk -v api_enable="$API_ENABLE" -v api_address="$API_ADDR" -v grpc_enable="$GRPC_ENABLE" -v grpc_address="$GRPC_ADDR" -v grpc_web_enable="$GRPC_WEB_ENABLE" '
   BEGIN {OFS=FS="="}
-  /^\[api\]$/ {api=1; grpc=0; grpc_web=0}
-  /^\[grpc\]$/ {grpc=1; api=0; grpc_web=0}
-  /^\[grpc-web\]$/ {grpc_web=1; api=0; grpc=0}
+  /^\[api\]$/ {api=1; grpc=0; grpc_web=0; rosetta=0}
+  /^\[grpc\]$/ {grpc=1; api=0; grpc_web=0; rosetta=0}
+  /^\[grpc-web\]$/ {grpc_web=1; api=0; grpc=0; rosetta=0}
+  /^\[rosetta\]$/ {rosetta=1; api=0; grpc=0; grpc_web=0}
   api && $1~/enable/ {$2=" " api_enable}
-  api && $1~/address/ {$2=" \"" api_address "\""}
+  api && $1~/address/ && !rosetta {$2=" \"" api_address "\""}
   grpc && $1~/enable/ {$2=" " grpc_enable}
   grpc && $1~/^address/ {$2=" \"" grpc_address "\""}
   grpc_web && $1~/^enable/ {$2=" " grpc_web_enable}
